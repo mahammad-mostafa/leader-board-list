eabcd44909ce68c2e9b651981d60cb9f2d85114a
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2023-07-27 21:52:51 +0300

    Attached event listeners & handlers

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
+import Network from './modules/network.js';
 import './index.css';
+
+const network = new Network(document.querySelector('.list ul'));
+const refreshElement = document.querySelector('.list button');
+const insertElement = document.querySelector('.form form');
+
+const buttonEvent = () => {
+  network.loadList();
+};
+
+const insertEvent = (event) => {
+  event.preventDefault();
+  network.insertPlayer({ user: insertElement.name.value, score: insertElement.score.value });
+  insertElement.reset();
+};
+
+refreshElement.addEventListener('click', buttonEvent);
+insertElement.addEventListener('submit', insertEvent);
